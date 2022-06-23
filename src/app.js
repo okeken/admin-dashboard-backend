951268669bf9ff9cb3583bc337e12b14b4875dc1
okeken <okekehinde@gmail.com>
2022-06-23 21:37:29 +0200

    vercel test

--- src/app.js
+++ src/app.js
@@ -7,10 +7,22 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 server.db = router.db;
 const middlewares = jsonServer.defaults();
 
-
 server.use(middlewares);
+
+const rules = auth.rewriter({
+  // Permission rules
+  products: 660,
+  brands: 440,
+});
+
+// You must apply the middlewares in the following order
+const port = process.env.PORT || 5000;
+server.use(rules);
 server.use(auth);
 server.use(router);
-server.listen(5000, () => {
-  console.log("JSON Server is running");
+server.listen(port, () => {
+  console.log("JSON Server is running on port " + port);
 });
+
+// Export the Express API
+module.exports = server;
